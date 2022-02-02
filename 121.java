c87f30255de54c3f5c6a10d2c4b369e82d8773ae
krut2001 <89316320+krut2001@users.noreply.github.com>
2022-02-03 02:44:58 +0530

    Create 121.java
    Best time to buy and sell stock

---
+++ 121.java
@@ -0,0 +1,18 @@
+//Best time to buy and sell stock
+//sliding window approach
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l=0;
+        int r=1;
+        int maxp=0;
+        while(r<prices.length){
+            if(prices[l]<prices[r]){
+                int profit=prices[r]-prices[l];
+                maxp=Integer.max(maxp,profit);
+            }
+            else l=r;
+            r+=1;
+        }
+        return maxp;
+    }
+}
